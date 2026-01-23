f21e05dc26d8243376c1210db9f5a4a9ddbd7690
brynjar <brynjar@drageide.com>
2026-01-23 13:12:06 +0100

    dhcp fungerte

--- testfioler/add feacher/dhcp.ps1
+++ testfioler/add feacher/dhcp.ps1
@@ -1,6 +1,16 @@
 Install-WindowsFeature -Name DHCP -IncludeManagementTools
 Import-Module DHCPServer
 
+# Configure a static IP address for the DHCP server
+$eth2ip = Read-Host "read the ip adress you want to set for the dhcp server"
+scopeGateway = Read-Host "Enter the default gateway for the DHCP scope and dhcp server (e.g.,"
+$scopeDnsServers = Read-Host "Enter the DNS server IP addresses for the DHCP scope and dhcp server, separated by commas (e.g.,"
+
+Set-NetIPInterface -InterfaceAlias "Ethernet 2" -Dhcp Disabled
+New-NetIPAddress -InterfaceAlias "Ethernet 2" -IPAddress $eth2ip -PrefixLength 24 -DefaultGateway "192.168.1.1"
+Set-DnsClientServerAddress -InterfaceAlias "Ethernet 2" -ServerAddresses ("127.0.0.1",$scopeDnsServers.Split(","))
+
+
 # Authorize the DHCP server in Active Directory
 $dhcpServerIp = (Get-NetIPAddress -InterfaceAlias "Ethernet" | Where-Object {$_.AddressFamily -eq "IPv4"}).IPAddress
 Add-DhcpServerInDC -DnsName $dhcpServerIp -IpAddress $dhcpServerIp
@@ -8,9 +18,6 @@ Add-DhcpServerInDC -DnsName $dhcpServerIp -IpAddress $dhcpServerIp
 $scopeName = "Office Network"
 $scopeStartIp = Read-Host "Enter the start IP address for the DHCP scope (e.g.,"
 $scopeEndIp = Read-Host "Enter the end IP address for the DHCP scope (e.g.,"
-$scopeSubnetMask = Read-Host "Enter the subnet mask for the DHCP scope (e.g.,"
-$scopeGateway = Read-Host "Enter the default gateway for the DHCP scope (e.g.,"
-$scopeDnsServers = Read-Host "Enter the DNS server IP addresses for the DHCP scope, separated by commas (e.g.,"
 Add-DhcpServerv4Scope -Name $scopeName -StartRange $scopeStartIp -EndRange $scopeEndIp -SubnetMask $scopeSubnetMask -State Active
 Set-DhcpServerv4OptionValue -ScopeId $scopeStartIp -Router $scopeGateway -DnsServer $scopeDnsServers
 Write-Host "DHCP server configured successfully."
